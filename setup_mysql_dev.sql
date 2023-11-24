66ffd81aa9f6ee6e3fb5e2d22ef04132b77dd201
Daedalus <manny.is.daedalus@pm.me>
2023-11-24 21:27:11 +0000

    added comments to sql file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,6 @@
+-- create new database and new user and assign all privileges to the new
+-- user
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
