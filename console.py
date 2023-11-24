b44ca17e5cb585d75bf5eccc5b23103715b11669
El-gibbor <chiagoziem11@yahoo.com>
2023-11-25 00:51:53 +0400

    updated create method in console.py

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -114,17 +114,41 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class"""
-        if not args:
+        """Create an object of a specified class with given parameters."""
+        list_of_args = args.split()
+
+        if not list_of_args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        if list_of_args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+
+        new_instance = HBNBCommand.classes[list_of_args[0]]()
+
+        # iterate over the remaining args (attr="value")
+        for idx, attr in enumerate(list_of_args[1:], 1):
+            # split into attr name & value, grab by idx ('attr', '=', 'val')
+            obj_attr = list_of_args[idx].partition("=")
+            attr_name, value = obj_attr[0], obj_attr[2]
+
+            # handle quoted attribute value and underscore
+            if '\"' in value:
+                value = value[1:-1].replace('_', ' ')
+            # handle digits as int and value with dot as float
+            if '.' in value:
+                value = float(value)
+                print(value)
+                print(type(value))
+            else:
+                try:
+                    value = int(value)
+                except ValueError:
+                    pass
+            if hasattr(new_instance, attr_name):
+                setattr(new_instance, attr_name, value)
         storage.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +296,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +304,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] !=  ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
---
+++ g.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+class kk:
+    pass
+
+print(type(kk))
