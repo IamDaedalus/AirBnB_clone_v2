af789b09b20d097940c1db38f7ba4a67554627fc
El-gibbor <chiagoziem11@yahoo.com>
2023-12-22 14:42:57 +0400

    addressed a syntax error

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object of DBstorage or FileStorage"""
 
 from os import environ
 
 if environ['HBNB_TYPE_STORAGE'] == 'db':
     from models.engine.db_storage import DBstorage
     storage = DBstorage()
-    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+storage.reload()
