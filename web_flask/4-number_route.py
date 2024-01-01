25805e4b9818844874899cf7f939fcbfed63a4ac
Daedalus <manny.is.daedalus@pm.me>
2024-01-01 23:44:01 +0000

    added first few web_flask tasks

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """This method prints Hello HBNB! upon request"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Method prints HBNB upon request"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """
+    This method replaces text with whatever text is passed to it. Refer to
+    https://flask.palletsprojects.com/en/2.3.x/quickstart/#variable-rules
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """
+    this method replaces <text> with the contents of text or 'is cool' by def
+    https://flask.palletsprojects.com/en/2.3.x/quickstart/#variable-rules
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_number(n):
+    """This method only returns the right page if n is an integer and nothing
+    else"""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
